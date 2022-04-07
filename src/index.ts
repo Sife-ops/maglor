ed03af7526ca332274a63b3fea14584437a27ee0
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-04-06 18:40:27 -0700

    child process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('sup')
+import { exec } from "child_process";
+
+const main = async () => {
+  exec("bw status", (error, stdout, stderr) => {
+    const json = JSON.parse(stdout);
+    console.log(json);
+  });
+};
+
+main();
